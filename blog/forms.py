033612ea0b199393f32f2978d5f577e79a0fde49
Fatima hdez <fa.hikariangel@gmail.com>
2018-05-09 21:34:04 -0500

    a,b,c, edicion de post/blog

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title','text',)
+        labels ={
+        'title': 'Titulo',
+        'text':'Articulo',
+        }
